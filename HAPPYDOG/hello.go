5a6bfa7410f8e3488fbe6f3ce9b4b268e1faeece
raptemple <raptemple@hotmail.com>
2019-12-19 12:52:10 -0600

    cleanup and updated tests

--- HAPPYDOG/hello.go
+++ HAPPYDOG/hello.go
@@ -1,7 +1,29 @@
-package hello
+package main
 
-import "rsc.io/quote"
+import (
+  "fmt"
+  "rsc.io/quote"
+)
+
+func main() {
+  fmt.Println(Hello())
+  fmt.Println(OptQuote()) //return quote.Opt()
+  fmt.Println(GlassQuote()) //return quote.Glass()
+  fmt.Println(GoQuote()) //return quote.Go()
+}
 
 func Hello() string {
     return quote.Hello()
+}
+
+func OptQuote() string {
+	return quote.Opt()
+}
+
+func GlassQuote() string {
+	return quote.Glass()
+}
+
+func GoQuote() string {
+	return quote.Go()
 }
--- HAPPYDOG/hello_test.go
+++ HAPPYDOG/hello_test.go
@@ -1,4 +1,4 @@
-package hello
+package main
 
 import "testing"
 
@@ -6,5 +6,26 @@ func TestHello(t *testing.T) {
     want := "Hello, world."
     if got := Hello(); got != want {
         t.Errorf("Hello() = %q, want %q", got, want)
+    }
+}
+
+func TestOptQuote(t *testing.T) {
+    want := "If a program is too slow, it must have a loop."
+    if got := OptQuote(); got != want {
+        t.Errorf("OptQuote() = %q, want %q", got, want)
+    }
+}
+
+func TestGlassQuote(t *testing.T) {
+    want := "I can eat glass and it doesn't hurt me."
+    if got := GlassQuote(); got != want {
+        t.Errorf("OptQuote() = %q, want %q", got, want)
+    }
+}
+
+func TestGoQuote(t *testing.T) {
+    want := "Don't communicate by sharing memory, share memory by communicating."
+    if got := GoQuote(); got != want {
+        t.Errorf("OptQuote() = %q, want %q", got, want)
     }
 }
